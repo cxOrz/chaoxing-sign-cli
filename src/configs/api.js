3f02c361f180953d31d71e664011d724f3199f47
cxOrz <cxOrz@outlook.com>
2021-10-25 18:45:14 +0800

    架子搭好

---
+++ src/configs/api.js
@@ -0,0 +1,10 @@
+module.exports = {
+  LOGIN_PAGE: {
+    URL: 'http://passport2.chaoxing.com/login?fid=&newversion=true&refer=http%3A%2F%2Fi.chaoxing.com',
+    METHOD: 'GET'
+  },
+  LOGIN: {
+    URL: 'http://passport2.chaoxing.com/fanyalogin',
+    METHOD: 'POST',
+  }
+}
