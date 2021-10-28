9e524afd6eab356f067a6c658d1ebfb35ec44a05
cxOrz <cxOrz@outlook.com>
2021-10-28 23:01:37 +0800

    修复活动检测bug

--- src/functions/activity.js
+++ src/functions/activity.js
@@ -6,7 +6,7 @@ const { ACTIVELIST } = require("../configs/api")
 exports.getSignActivity = async (courses, uf, _d, UID, vc3) => {
   console.log('正在查询有效签到活动，等待时间视网络情况而定...')
   return new Promise(async (resolve_) => {
-    let i = null
+    let i = null, aid
     for (i = 0; i < courses.length; i++) {
       await new Promise((resolve) => {
         let data = ''
@@ -23,16 +23,19 @@ exports.getSignActivity = async (courses, uf, _d, UID, vc3) => {
               // 活动开始超过一小时则忽略
               if ((new Date().getTime() - data.data.activeList[0].startTime) / 1000 < 3600) {
                 console.log(`检测到活动：${data.data.activeList[0].nameOne}`)
-                // 传出签到活动 aid
-                resolve(i = 1000) // 设置flag结束循环
+                aid = data.data.activeList[0].id
+                i = 999 // 设置flag结束循环，设为非length值，说明是获取到了活动而结束循环，设置完最后还会自增一次。
+                resolve()
                 resolve_(data.data.activeList[0].id) // 传出活动id
-              } else resolve(null)
-            } else resolve(null)
+              }
+            }
+            resolve()
           })
         })
       })
     }
-    if (i >= courses.length) {
+    // 若等于length说明是遍历了全部，都没有获得活动
+    if (i == courses.length) {
       console.log('未检测到有效签到活动！')
       process.exit(0)
     }
