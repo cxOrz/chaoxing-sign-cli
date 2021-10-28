b1f18bff39e80648bdd34d4befe0e5c960b9163c
cxOrz <cxOrz@outlook.com>
2021-10-28 15:05:58 +0800

    添加npm命令；完成签到活动获取；完成用户名信息获取；完成二维码签到；精简修正cookie传递；

--- src/functions/QRCode.js
+++ src/functions/QRCode.js
@@ -1,12 +1,20 @@
 const https = require('https')
 const { PPTSIGN } = require('../configs/api')
 
-exports.QRCodeSign = (enc, name, fid, uid, aid) => {
-  let data = ''
-  https.get(PPTSIGN.URL + `?enc=${enc}&name=${encodeURI(name)}&activeId=${aid}&uid=${uid}&clientip=&useragent=&latitude=-1&longitude=-1&fid=${fid}&appType=15`, (res) => {
-    res.on('data', (chunk) => { data += chunk })
-    res.on('end', () => {
-      console.log(data)
+exports.QRCodeSign = async (enc, name, fid, uid, aid, uf, _d, vc3) => {
+  return new Promise((resolve) => {
+    let data = ''
+    https.get(PPTSIGN.URL + `?enc=${enc}&name=${encodeURI(name)}&activeId=${aid}&uid=${uid}&clientip=&useragent=&latitude=-1&longitude=-1&fid=${fid}&appType=15`, {
+      headers: {
+        'Cookie': `uf=${uf}; _d=${_d}; UID=${uid}; vc3=${vc3};`
+      }
+    }, (res) => {
+      res.on('data', (chunk) => { data += chunk })
+      res.on('end', () => {
+        if (data == 'success')
+          console.log(`${data}签到成功`)
+          resolve()
+      })
     })
   })
 }
