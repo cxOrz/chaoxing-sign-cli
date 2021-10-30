0b56fc0971d433a6da6177a48fb54ca592530440
cxOrz <cxOrz@outlook.com>
2021-10-30 19:59:17 +0800

    完成位置签到；更新readme

---
+++ src/functions/location.js
@@ -0,0 +1,20 @@
+const https = require('https')
+const { PPTSIGN } = require('../configs/api')
+
+exports.LocationSign = async (uf, _d, vc3, name, address, activeId, uid, lat, lon, fid) => {
+  let data = ''
+  return new Promise((resolve) => {
+    https.get(PPTSIGN.URL + `?name=${name}&address=${address}&activeId=${activeId}&uid=${uid}&clientip=&latitude=${lat}&longitude=${lon}&fid=${fid}&appType=15&ifTiJiao=1`, {
+      headers: {
+        'Cookie': `uf=${uf}; _d=${_d}; UID=${uid}; vc3=${vc3};`
+      }
+    }, (res) => {
+      res.on('data', (chunk) => { data += chunk })
+      res.on('end', () => {
+        if (data == 'success')
+          console.log(`${data}签到成功`)
+        resolve()
+      })
+    })
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { getSignActivity } = require("./functions/activity");
+const { LocationSign } = require("./functions/location");
 const { QRCodeSign } = require("./functions/QRCode");
 const { userLogin, getCourses, getAccountInfo } = require("./functions/user");
 const readline = require('./utils/readline')
@@ -19,11 +20,22 @@ const rl = readline.createInterface()
   let courses = await getCourses(params._uid, params._d, params.vc3)
   // 获取进行中的签到活动id
   let aid = await getSignActivity(courses, params.uf, params._d, params._uid, params.vc3)
-  
-  // 二维码签到
-  if (aid != null && process.argv.includes('--qrcode')) {
-    let enc = await readline.question(rl, 'enc(微信或其他识别二维码，可得enc参数)：')
-    await QRCodeSign(enc, name, params.fid, params._uid, aid, params.uf, params._d, params.vc3)
-    process.exit(0)
+
+  // 检测到签到活动
+  if (aid != null) {
+    // 二维码签到
+    if (process.argv.includes('--qrcode')) {
+      let enc = await readline.question(rl, 'enc(微信或其他识别二维码，可得enc参数)：')
+      await QRCodeSign(enc, name, params.fid, params._uid, aid, params.uf, params._d, params.vc3)
+      process.exit(0)
+    }
+    // 位置签到
+    if (process.argv.includes('--location')) {
+      console.log('https://api.map.baidu.com/lbsapi/getpoint/index.html')
+      let lnglat = await readline.question(rl, '经纬度，如\"113.516288,34.817038\": ')
+      let address = await readline.question(rl, '详细地址: ')
+      await LocationSign(params.uf, params._d, params.vc3, name, address, aid, params._uid, Number(lnglat.substring(lnglat.indexOf(',') + 1, lnglat.length)), Number(lnglat.substring(0, lnglat.indexOf(','))), params.fid)
+      process.exit(0)
+    }
   }
 }()
