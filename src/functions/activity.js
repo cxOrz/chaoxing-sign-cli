d933bb35b77f4bf1cf79d899b5853eef85f34dec
cxOrz <cxOrz@outlook.com>
2022-03-17 22:23:06 +0800

    优化获取活动代码；合并命令，自动判断签到方式；修改主页说明；修改Termux说明

--- src/functions/activity.js
+++ src/functions/activity.js
@@ -1,47 +1,48 @@
-const { getCourses } = require("./user")
 const https = require('https')
 const { ACTIVELIST } = require("../configs/api")
 
 // 返回签到活动
 exports.getSignActivity = async (courses, uf, _d, UID, vc3) => {
   console.log('正在查询有效签到活动，等待时间视网络情况而定...')
-  return new Promise(async (resolve_) => {
-    let i = null, aid
-    for (i = 0; i < courses.length; i++) {
-      await new Promise((resolve) => {
-        let data = ''
-        https.get(ACTIVELIST.URL + `?fid=0&courseId=${courses[i].courseId}&classId=${courses[i].classId}&_=${new Date().getTime()}`, {
-          headers: {
-            'Cookie': `uf=${uf}; _d=${_d}; UID=${UID}; vc3=${vc3};`,
-          }
-        }, (res) => {
-          res.on('data', (chunk) => { data += chunk })
-          res.on('end', () => {
-            // console.log(data)
-            data = JSON.parse(data)
-            if (data.data.activeList.length != 0) {
-              let otherId = Number(data.data.activeList[0].otherId)
-              // 判断是否有效签到活动
-              if ((otherId >= 0 && otherId <= 5) && data.data.activeList[0].status == 1) {
-                // 活动开始超过一小时则忽略
-                if ((new Date().getTime() - data.data.activeList[0].startTime) / 1000 < 7200) {
-                  console.log(`检测到活动：${data.data.activeList[0].nameOne}`)
-                  aid = data.data.activeList[0].id
-                  i = 999 // 设置flag结束循环，设为非length值，说明是获取到了活动而结束循环，设置完最后还会自增一次。
-                  resolve()
-                  resolve_(data.data.activeList[0].id) // 传出活动id
+  let i = null, activity = null
+  for (i = 0; i < courses.length; i++) {
+    // 循环异步请求
+    await new Promise((resolve) => {
+      let data = ''
+      https.get(ACTIVELIST.URL + `?fid=0&courseId=${courses[i].courseId}&classId=${courses[i].classId}&_=${new Date().getTime()}`, {
+        headers: {
+          'Cookie': `uf=${uf}; _d=${_d}; UID=${UID}; vc3=${vc3};`,
+        }
+      }, (res) => {
+        res.on('data', (chunk) => { data += chunk })
+        res.on('end', () => {
+          // console.log(data)
+          data = JSON.parse(data)
+          if (data.data.activeList.length != 0) {
+            let otherId = Number(data.data.activeList[0].otherId)
+            // 判断是否有效签到活动
+            if ((otherId >= 0 && otherId <= 5) && data.data.activeList[0].status == 1) {
+              // 活动开始超过一小时则忽略
+              if ((new Date().getTime() - data.data.activeList[0].startTime) / 1000 < 7200) {
+                console.log(`检测到活动：${data.data.activeList[0].nameOne}`)
+                activity = {
+                  aid: data.data.activeList[0].id,
+                  otherId
                 }
+                i = NaN // 设为NaN将结束循环，该值说明获取到了活动。
               }
             }
-            resolve()
-          })
+          }
+          resolve() // 每结束一个课程的活动判断
         })
       })
-    }
-    // 若等于length说明是遍历了全部，都没有获得活动
-    if (i == courses.length) {
-      console.log('未检测到有效签到活动！')
-      process.exit(0)
-    }
-  })
+    })
+  }
+  // 若等于length说明是遍历了全部，都没有获得活动
+  if (i == courses.length) {
+    console.log('未检测到有效签到活动！')
+    process.exit(0) // 直接退出
+  } else {
+    return activity
+  }
 }
--- src/index.js
+++ src/index.js
@@ -29,11 +29,6 @@ const rl = readline.createInterface()
     } else {
       // 使用本地储存的参数
       const data = getStore()
-      
-      // if ((Date.now() - new Date(data.users[Number(input)].date)) / 86400000 >= 7) {
-      //   console.log('身份过期，程序将关闭，请你使用手动填写用户名密码的方式登录！手动登录后身份信息刷新，之后可继续使用本地凭证！')
-      //   process.exit(0)
-      // }
       params = data.users[Number(input)].params
     }
   }
@@ -44,36 +39,47 @@ const rl = readline.createInterface()
 
   // 获取所有课程
   let courses = await getCourses(params._uid, params._d, params.vc3)
-  // 获取进行中的签到活动id
-  let aid = await getSignActivity(courses, params.uf, params._d, params._uid, params.vc3)
+  // 获取进行中的签到活动
+  let activity = await getSignActivity(courses, params.uf, params._d, params._uid, params.vc3)
 
   // 检测到签到活动
-  if (aid != null) {
-    // 二维码签到
-    if (process.argv.includes('--qrcode')) {
+  switch (activity.otherId) {
+    case 2: {
+      // 二维码签到
       let enc = await readline.question(rl, 'enc(微信或其他识别二维码，可得enc参数)：')
-      await QRCodeSign(enc, name, params.fid, params._uid, aid, params.uf, params._d, params.vc3)
+      await QRCodeSign(enc, name, params.fid, params._uid, activity.aid, params.uf, params._d, params.vc3)
       process.exit(0)
     }
-    // 位置签到
-    if (process.argv.includes('--location')) {
+    case 4: {
+      // 位置签到
       console.log('https://api.map.baidu.com/lbsapi/getpoint/index.html')
       let lnglat = await readline.question(rl, '经纬度(如113.516288,34.817038): ')
       let address = await readline.question(rl, '详细地址: ')
-      await LocationSign(params.uf, params._d, params.vc3, name, address, aid, params._uid, Number(lnglat.substring(lnglat.indexOf(',') + 1, lnglat.length)), Number(lnglat.substring(0, lnglat.indexOf(','))), params.fid)
+      await LocationSign(params.uf, params._d, params.vc3, name, address, activity.aid, params._uid, Number(lnglat.substring(lnglat.indexOf(',') + 1, lnglat.length)), Number(lnglat.substring(0, lnglat.indexOf(','))), params.fid)
       process.exit(0)
     }
-    // 普通签到、手势签到
-    if (process.argv.includes('--general')) {
-      await GeneralSign(params.uf, params._d, params.vc3, name, aid, params._uid, params.fid)
+    case 3: {
+      // 手势签到
+      await GeneralSign(params.uf, params._d, params.vc3, name, activity.aid, params._uid, params.fid)
       process.exit(0)
     }
-    // 拍照签到
-    if (process.argv.includes('--photo')) {
-      await readline.question(rl, '访问 https://pan-yz.chaoxing.com 并在根目录上传你想要提交的照片，格式为jpg或png，命名为 0.jpg 或 0.png，完成后按回车继续...')
-      // 获取照片objectId
-      let objectId = await getObjectIdFromcxPan(params.uf, params._d, params.vc3, params._uid)
-      await PhotoSign(params.uf, params._d, params.vc3, name, aid, params._uid, params.fid, objectId)
+    case 5: {
+      // 签到码签到
+      await GeneralSign(params.uf, params._d, params.vc3, name, activity.aid, params._uid, params.fid)
+      process.exit(0)
+    }
+    case 0: {
+      let photo = await readline.question(rl, '[注意]是否为拍照签到(y是, n否): ')
+      if (photo === 'y') {
+        // 拍照签到
+        await readline.question(rl, '访问 https://pan-yz.chaoxing.com 并在根目录上传你想要提交的照片，格式为jpg或png，命名为 0.jpg 或 0.png，完成后按回车继续...')
+        // 获取照片objectId
+        let objectId = await getObjectIdFromcxPan(params.uf, params._d, params.vc3, params._uid)
+        await PhotoSign(params.uf, params._d, params.vc3, name, activity.aid, params._uid, params.fid, objectId)
+      } else {
+        // 普通签到
+        await GeneralSign(params.uf, params._d, params.vc3, name, activity.aid, params._uid, params.fid)
+      }
       process.exit(0)
     }
   }
