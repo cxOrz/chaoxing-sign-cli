858b37ca625565cf2106e6e637fface0d6ac86a6
cxOrz <miaochenxi@outlook.com>
2022-09-01 17:55:58 +0800

    扩展globalThis，处理JSON解析异常

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -6,4 +6,21 @@ function delay(timeout: number = 0) {
   return new Promise<void>(res => setTimeout(() => res(), timeout));
 }
 
-export { delay };
+function parseJSON(text: string): any {
+  try {
+    return JSON.parse(text);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(err);
+      console.log('\nInvalid JSON: ' + text)
+    }
+  }
+}
+
+globalThis.JSON.parseJSON = parseJSON
+
+function extendGlobalThis(gl: any) {
+  gl.JSON.parseJSON = parseJSON;
+}
+
+export { delay, extendGlobalThis };
