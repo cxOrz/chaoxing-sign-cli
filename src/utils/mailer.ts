85fead8ee970ab7dbefb42ecdbc22c0f88010c3d
cxOrz <cxOrz@outlook.com>
2022-09-08 22:44:57 +0800

    自动区分普通和拍照签到；修改邮件发送函数；

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,21 +1,27 @@
 import nodemailer from 'nodemailer';
-import { getJsonObject } from "./file";
 
-const storage = getJsonObject('configs/storage.json');
+interface MailConfig {
+  host: string;
+  port: number;
+  ssl: boolean;
+  user: string;
+  pass: string;
+  to: string;
+}
 
-export function sendEmail(aid: string, uid: string, realname: string, status: string) {
+export function sendEmail(aid: string, uid: string, realname: string, status: string, mailing: MailConfig) {
   let transporter = nodemailer.createTransport({
-    host: storage.mailing.host,
-    port: storage.mailing.port,
-    secure: storage.mailing.ssl,
+    host: mailing.host,
+    port: mailing.port,
+    secure: mailing.ssl,
     auth: {
-      user: storage.mailing.user,
-      pass: storage.mailing.pass,
+      user: mailing.user,
+      pass: mailing.pass,
     },
   });
   transporter.sendMail({
-    from: `"CLI" <${storage.mailing.user}>`,
-    to: storage.mailing.to,
+    from: `"CLI" <${mailing.user}>`,
+    to: mailing.to,
     subject: "服务器签到反馈",
     html: `<table border="1"><thead><th>aid</th><th>uid</th><th>name</th><th>status</th></thead><tbody><td>${aid}</td><td>${uid}</td><td>${realname}</td><td>${status}</td></tbody></table>`
   }, () => { transporter.close() });
