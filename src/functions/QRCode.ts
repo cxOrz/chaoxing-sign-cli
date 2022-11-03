4063345791d531c10e12d1af75fb7d50f92398e5
Void <computeridiot999@gmail.com>
2022-11-04 04:16:50 +0800

    自动转换网址到enc

--- src/functions/QRCode.ts
+++ src/functions/QRCode.ts
@@ -1,9 +1,15 @@
 import https from 'https';
 import { PPTSIGN } from '../configs/api';
 
+function parseUrl(url: string) {
+  if (url.startsWith(`https://passport2.chaoxing.com`)) url = new URLSearchParams(url).get('refer') ?? '';
+  return new URLSearchParams(url).get('enc') ?? '';
+}
+
 export const QRCodeSign = async (enc: string, name: string, fid: string, uid: string, aid: string | number, uf: string, _d: string, vc3: string) => {
   return new Promise((resolve) => {
     let data = '';
+    if (enc.startsWith('http')) enc = parseUrl(enc);
     https.get(PPTSIGN.URL + `?enc=${enc}&name=${encodeURI(name)}&activeId=${aid}&uid=${uid}&clientip=&useragent=&latitude=-1&longitude=-1&fid=${fid}&appType=15`, {
       headers: {
         'Cookie': `uf=${uf}; _d=${_d}; UID=${uid}; vc3=${vc3};`
