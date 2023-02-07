acc2e3018258a2bd63da0fa0da9402edfc7c39a6
cxOrz <cxOrz@outlook.com>
2023-02-07 11:24:59 +0800

    构建时忽略JS文件；新增 copyFiles 函数；

---
+++ src/utils/copyFiles.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import { copyFile } from 'fs/promises';
+
+type targetToDestArrayType = { from: string; to: string }[];
+
+/**
+ * @param targetToDestArray 传入数组 [{ from: string; to: string }...]
+ */
+export const copyFiles = async (targetToDestArray: targetToDestArrayType) => {
+  for await (const item of targetToDestArray) {
+    await copyFile(item.from, item.to, fs.constants.COPYFILE_EXCL);
+  }
+};
