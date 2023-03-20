e9d9f72d94a2330862af7d40f25947cd8fee6a5d
cxOrz <cxOrz@outlook.com>
2023-03-21 01:23:43 +0800

    添加ESLint配置；新增lint命令；

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,4 +1,46 @@
 module.exports = {
-  extends: ["turbo", "prettier"],
-  rules: {},
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'turbo',
+    'prettier'
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2,
+      {
+        'SwitchCase': 1
+      }
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
 };
