eb8d573b11372936a911166aa5b53c63a4bada0a
SummonR7000P <2239760702@qq.com>
2023-03-23 20:16:21 +0800

    Add go-cqhttp support

--- apps/server/src/functions/tencent.qrcode.ts
+++ apps/server/src/functions/tencent.qrcode.ts
@@ -20,3 +20,29 @@ export const QrCodeScan = async (base64str: string) => {
     ImageBase64: base64str,
   });
 };
+
+export const urlQrCodeScan = (imgurlstr: string) => {
+  return new Promise((res, rej) => {
+    const client = new OcrClient({
+      credential: {
+        secretId: ENVJSON.tencent.secretId,
+        secretKey: ENVJSON.tencent.secretKey
+      },
+      region: "ap-shanghai",
+      profile: {
+        httpProfile: {
+          endpoint: "ocr.tencentcloudapi.com",
+        },
+      },
+    });
+    client.QrcodeOCR({
+      "ImageUrl": imgurlstr
+    }).then(
+      (data: any) => {
+        res(data);
+      }, (err: any) => {
+        rej(err);
+      }
+    );
+  });
+};
