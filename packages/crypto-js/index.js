5be97f98ab57aaca6e7760ed9b881a7a44172289
cxOrz <miaochenxi@outlook.com>
2023-04-02 21:26:50 +0800

    修复构建失败问题；

--- packages/crypto-js/index.js
+++ packages/crypto-js/index.js
@@ -1,3 +1,3 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const cryptojs = require('crypto-js');
-module.exports = cryptojs;
+import CryptoJS from 'crypto-js';
+export default CryptoJS;
